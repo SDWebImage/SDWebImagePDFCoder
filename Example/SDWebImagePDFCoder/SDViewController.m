740c36d6d7ed3f835a5297ea74c2969ef04925c4
DreamPiggy <lizhuoli1126@126.com>
2019-05-12 14:44:31 +0800

    Support to export the PDF data for iOS && macOS. iOS need iOS 11+ for keeping vector format.

--- Example/SDWebImagePDFCoder/SDViewController.m
+++ Example/SDWebImagePDFCoder/SDViewController.m
@@ -42,6 +42,8 @@
     [imageView1 sd_setImageWithURL:pdfURL placeholderImage:nil options:SDWebImageRetryFailed completed:^(UIImage * _Nullable image, NSError * _Nullable error, SDImageCacheType cacheType, NSURL * _Nullable imageURL) {
         if (image) {
             NSLog(@"PDF load success");
+            NSData *pdfData = [image sd_imageDataAsFormat:SDImageFormatPDF];
+            NSAssert(pdfData.length > 0, @"PDF Data export failed");
         }
     }];
     [imageView2 sd_setImageWithURL:pdfURL2 placeholderImage:nil options:SDWebImageRetryFailed context:@{SDWebImageContextPDFImageSize : @(imageView2.bounds.size)} progress:nil completed:^(UIImage * _Nullable image, NSError * _Nullable error, SDImageCacheType cacheType, NSURL * _Nullable imageURL) {
